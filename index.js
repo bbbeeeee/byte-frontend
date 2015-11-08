28d940b364403ccf1085cef724ed97bc26cb8d69
Brandon <brandontruong@udacity.com>
2015-11-08 04:01:38 -0500

    port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var bodyParser = require('body-parser');
 var config = require('./config/config');
 var express = require('express');
-
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,6 +20,6 @@ app.get('/feed', (req, res) => {
   res.sendfile('public/feed.html');
 });
 
-app.listen(config.serverPort, () => {
-  console.log(`Server now running at localhost:${config.serverPort}`);
+app.listen(app.get('port'), () => {
+  console.log(`Server now running`);
 });
